b2fa7d11522851578f0252ae9fe0c93fe4d994a9
Nisrine2001 <nisrine.touijer@gmail.com>
2023-10-17 18:28:18 -0700

    handle_print

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- *is written by nisrine and hajar
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
